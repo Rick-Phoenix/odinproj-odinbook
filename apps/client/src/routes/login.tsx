5d712f20c405aaf2a3f9fa5734c2bb2376f31850
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-01-14 20:24:17 +0100

    Login page tweak

--- apps/client/src/routes/login.tsx
+++ apps/client/src/routes/login.tsx
@@ -1,16 +1,79 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { LoginForm } from "../components/login-form";
+import { Label } from "@radix-ui/react-label";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { useForm } from "@tanstack/react-form";
+import { zodSchemas } from "@nexus/shared-schemas";
 
 export const Route = createFileRoute("/login")({
   component: LoginPage,
 });
 
 function LoginPage() {
+  const form = useForm({
+    validators: {
+      onChange: zodSchemas.loginValidationSchema,
+    },
+  });
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
+    <LoginForm>
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="grid gap-2">
+            <form.Field
+              name="username"
+              children={(field) => {
+                return (
+                  <>
+                    <Label htmlFor={field.name}>Username</Label>
+                    <Input
+                      name={field.name}
+                      type="text"
+                      value={field.state.value}
+                      placeholder="Username"
+                      onBlur={field.handleBlur}
+                      required
+                    />
+                  </>
+                );
+              }}
+            ></form.Field>
+          </div>
+          <div className="grid gap-2">
+            <form.Field
+              name="password"
+              children={(field) => {
+                return (
+                  <>
+                    <Label htmlFor={field.name}>Password</Label>
+                    <Input
+                      name={field.name}
+                      type="password"
+                      value={field.state.value}
+                      placeholder="Password"
+                      onBlur={field.handleBlur}
+                      required
+                    />
+                  </>
+                );
+              }}
+            ></form.Field>
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <a href="#" className="underline underline-offset-4">
+            Sign up
+          </a>
+        </div>
+      </form>
+    </LoginForm>
   );
 }
