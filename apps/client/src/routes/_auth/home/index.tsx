6f6c3e9fcb5f360aace41ed4f6d20750c6dd793f
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-01-19 17:52:16 +0100

    - Isolated carousel element, added scrollarea for main section

--- apps/client/src/routes/_auth/home/index.tsx
+++ apps/client/src/routes/_auth/home/index.tsx
@@ -15,13 +15,13 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import { AnimatePresence } from "motion/react";
 import * as motion from "motion/react-client";
 import { useState } from "react";
-import { Button } from "../../../components/ui/button";
 import {
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "../../../components/ui/card";
+import { ScrollArea } from "../../../components/ui/scroll-area";
 
 // Zustand store
 
@@ -33,69 +33,84 @@ function RouteComponent() {
   const [isVisible, setIsVisible] = useState(true);
 
   return (
-    <main className="flex flex-1 flex-col gap-4 p-16 pb-6 pt-2">
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button variant={"ghost"}>
+    <ScrollArea
+      type="always"
+      className="h-full w-full flex-1 [&>[data-radix-scroll-area-viewport]]:max-h-[calc(100vh-200px)]"
+    >
+      <main className="flex flex-1 flex-col gap-4 p-16 pb-6 pt-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="p-3">
             <small className="text-sm font-medium leading-none">
               🔥 Trending
             </small>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem
-            onClick={() => {
-              setIsVisible(!isVisible);
-            }}
-          >
-            {isVisible ? "Hide" : "Show"}
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <AnimatePresence initial={false}>
-        {" "}
-        {isVisible ? (
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0 }}
-          >
-            <Carousel>
-              <CarouselContent>
-                <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
-                  <Link
-                    to={"/post"}
-                    className="flex aspect-video flex-col justify-end rounded-xl bg-muted/50"
-                  >
-                    {/* <div className="aspect-video rounded-xl bg-muted/50">opst 1</div> */}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              onClick={() => {
+                setIsVisible(!isVisible);
+              }}
+            >
+              {isVisible ? "Hide" : "Show"}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <TrendingCarousel isVisible={isVisible} />
+        <div className="flex min-h-[100vh] flex-1 flex-col justify-end rounded-xl bg-muted/50 md:min-h-min md:min-h-screen">
+          <CardContent>Blah</CardContent>
+          <CardHeader>
+            <CardTitle>Post 1</CardTitle>
+            <CardDescription>Post description</CardDescription>
+          </CardHeader>
+        </div>
+      </main>
+    </ScrollArea>
+  );
+}
 
-                    <CardContent>Blah</CardContent>
-                    <CardHeader>
-                      <CardTitle>Post 1</CardTitle>
-                      <CardDescription>Post description</CardDescription>
-                    </CardHeader>
-                  </Link>
-                </CarouselItem>
-                <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
-                  <div className="aspect-video rounded-xl bg-muted/50"></div>
-                </CarouselItem>
-                <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
-                  <div className="aspect-video rounded-xl bg-muted/50" />
-                </CarouselItem>
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
-          </motion.div>
-        ) : null}
-      </AnimatePresence>
-      <div className="flex min-h-[100vh] flex-1 flex-col justify-end rounded-xl bg-muted/50 md:min-h-min">
-        <CardContent>Blah</CardContent>
-        <CardHeader>
-          <CardTitle>Post 1</CardTitle>
-          <CardDescription>Post description</CardDescription>
-        </CardHeader>
-      </div>
-    </main>
+function TrendingCarousel({ isVisible }: { isVisible: boolean }) {
+  return (
+    <AnimatePresence initial={false}>
+      {isVisible ? (
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+        >
+          <Carousel opts={{ loop: true }}>
+            <CarouselContent>
+              <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+                <Link
+                  to={"/post"}
+                  className="flex aspect-video flex-col justify-end rounded-xl bg-muted/50"
+                >
+                  <CardContent>Blah</CardContent>
+                  <CardHeader>
+                    <CardTitle>Post 1</CardTitle>
+                    <CardDescription>Post description</CardDescription>
+                  </CardHeader>
+                </Link>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+                <div className="aspect-video rounded-xl bg-muted/50"></div>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+                <div className="aspect-video rounded-xl bg-muted/50"></div>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+                <div className="aspect-video rounded-xl bg-muted/50"></div>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+                <div className="aspect-video rounded-xl bg-muted/50"></div>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </motion.div>
+      ) : null}
+    </AnimatePresence>
   );
 }
