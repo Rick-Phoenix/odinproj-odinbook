d18d5eafb740096a608e605d54637c923b7b16f8
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-01-21 16:50:45 +0100

    Mid checkpoint

--- apps/client/src/routes/_app/rooms/posts/$postid.tsx
+++ apps/client/src/routes/_app/rooms/posts/$postid.tsx
@@ -16,18 +16,149 @@ export const Route = createFileRoute("/_app/rooms/posts/$postid")({
   component: RouteComponent,
 });
 
-const comments = [
+interface Comment {
+  text: string;
+  sub?: Comments;
+}
+
+type Comments = Comment[];
+
+function renderChildrenComments(comments: Comments, row: number) {
+  return comments.map((comment, i) => {
+    const currentCommentChildren = comments[i]?.sub;
+    return (
+      <>
+        <Separator
+          orientation="vertical"
+          className={`row-start-${row + i + 1} justify-self-center`}
+        />
+
+        <div
+          className={`col-start-2 row-start-${row + i + 1} grid grid-cols-[auto_1fr] items-center pt-4`}
+        >
+          <div className="relative col-start-1 row-start-1 h-10 w-10 rounded-full bg-foreground">
+            <span className="absolute -left-5 top-2 h-3 w-5 rounded-xl rounded-r-none rounded-t-none border-b border-l bg-transparent hover:border-b-white hover:border-l-white" />
+          </div>
+          <div className="col-start-2 row-start-1 flex flex-col pl-4">
+            <div className="flex gap-4">
+              <div>Nickname</div>
+              <div>Sent At</div>
+            </div>
+            <div>Role</div>
+          </div>
+
+          {currentCommentChildren && (
+            <Separator
+              orientation="vertical"
+              className="col-start-1 row-start-2 h-full justify-self-center"
+            />
+          )}
+
+          <div className="col-start-2 row-start-2 flex flex-col gap-2 pl-4 pt-4">
+            <div>Comment text</div>
+            <div className="flex items-center gap-2">
+              <PiThumbsUpBold /> <MessageCircleMore />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  });
+}
+
+function renderRootComments(comments: Comments) {
+  return comments.map((comment, i) => {
+    const previousCommentHadChildren =
+      i > 0 && comments[i - 1]?.sub ? true : false;
+    const currentCommentChildren = comments[i]?.sub;
+    const childrenCommentHasChildren =
+      Array.isArray(comments[i]?.sub) &&
+      comments[i].sub.some((comment) => {
+        return comment?.sub;
+      });
+    const startingRow = previousCommentHadChildren ? i + 2 : i + 1;
+    return (
+      <>
+        <div
+          key={i}
+          className={`col-start-1 col-end-3 row-start-${startingRow} grid grid-cols-[auto_1fr] items-center justify-self-start`}
+        >
+          <div className="col-start-1 row-start-1 h-10 w-10 rounded-full bg-foreground"></div>
+          <div className="col-start-2 row-start-1 flex flex-col pl-4">
+            <div className="flex gap-4">
+              <div>Nickname</div>
+              <div>Sent At</div>
+            </div>
+            <div>Role</div>
+          </div>
+          <Separator
+            orientation="vertical"
+            className={`col-start-1 row-start-2 h-full justify-self-center`}
+          />
+          <div
+            className={`col-start-2 row-start-2 flex flex-col gap-2 pl-4 pt-4`}
+          >
+            <div>Comment text</div>
+            <div className="flex items-center gap-2">
+              <PiThumbsUpBold /> <MessageCircleMore />
+            </div>
+          </div>
+        </div>
+        {currentCommentChildren ? (
+          <>
+            <Separator
+              orientation="vertical"
+              className={`row-start-${startingRow + 1} justify-self-center`}
+            />
+
+            <div
+              className={`col-start-2 row-start-${startingRow + 1} grid grid-cols-[auto_1fr] items-center pt-4`}
+            >
+              <div className="relative col-start-1 row-start-1 h-10 w-10 rounded-full bg-foreground">
+                <span className="absolute -left-5 top-2 h-3 w-5 rounded-xl rounded-r-none rounded-t-none border-b border-l bg-transparent hover:border-b-white hover:border-l-white" />
+              </div>
+              <div className="col-start-2 row-start-1 flex flex-col pl-4">
+                <div className="flex gap-4">
+                  <div>Nickname</div>
+                  <div>Sent At</div>
+                </div>
+                <div>Role</div>
+              </div>
+
+              {childrenCommentHasChildren && (
+                <Separator
+                  orientation="vertical"
+                  className="col-start-1 row-start-2 h-full justify-self-center"
+                />
+              )}
+
+              <div className="col-start-2 row-start-2 flex flex-col gap-2 pl-4 pt-4">
+                <div>Comment text</div>
+                <div className="flex items-center gap-2">
+                  <PiThumbsUpBold /> <MessageCircleMore />
+                </div>
+              </div>
+            </div>
+          </>
+        ) : null}
+      </>
+    );
+  });
+}
+
+const comments: Comments = [
   { text: " comment text", sub: [{ text: "comment text" }] },
   { text: " comment text" },
   { text: " comment text" },
   {
     text: " comment text",
-    sub: {
-      text: " comment text",
-      sub: { text: " comment text", sub: [{ text: "comment text" }] },
-    },
+    sub: [
+      {
+        text: " comment text",
+        sub: [{ text: " comment text", sub: [{ text: "comment text" }] }],
+      },
+    ],
   },
-  ,
   {
     text: " comment text",
     sub: [
@@ -84,83 +215,7 @@ function RouteComponent() {
       <div className="p-6">
         <div className="py-6">Comments</div>
         <div className="grid grid-cols-[2.5rem_1fr] items-center">
-          {comments.map((comment, i) => {
-            const previousCommentHadChildren =
-              i > 0 && comments[i - 1]?.sub ? true : false;
-            const currentCommentHasChildren = comments[i]?.sub || false;
-            const childrenCommentHasChildren =
-              Array.isArray(comments[i]?.sub) &&
-              comments[i].sub.some((comment) => {
-                return comment?.sub;
-              });
-            const gridOrder = previousCommentHadChildren ? i + 2 : i + 1;
-            return (
-              <>
-                <div
-                  key={i}
-                  className={`col-start-1 col-end-3 row-start-${gridOrder} grid grid-cols-[auto_1fr] items-center justify-self-start`}
-                >
-                  <div className="col-start-1 row-start-1 h-10 w-10 rounded-full bg-foreground"></div>
-                  <div className="col-start-2 row-start-1 flex flex-col pl-4">
-                    <div className="flex gap-4">
-                      <div>Nickname</div>
-                      <div>Sent At</div>
-                    </div>
-                    <div>Role</div>
-                  </div>
-                  <Separator
-                    orientation="vertical"
-                    className={`col-start-1 row-start-2 h-full justify-self-center`}
-                  />
-                  <div
-                    className={`col-start-2 row-start-2 flex flex-col gap-2 pl-4 pt-4`}
-                  >
-                    <div>Comment text</div>
-                    <div className="flex items-center gap-2">
-                      <PiThumbsUpBold /> <MessageCircleMore />
-                    </div>
-                  </div>
-                </div>
-                {currentCommentHasChildren ? (
-                  <>
-                    <Separator
-                      orientation="vertical"
-                      className={`row-start-${gridOrder + 1} justify-self-center`}
-                    />
-
-                    <div
-                      className={`col-start-2 row-start-${gridOrder + 1} grid grid-cols-[auto_1fr] items-center pt-4`}
-                    >
-                      <div className="relative col-start-1 row-start-1 h-10 w-10 rounded-full bg-foreground">
-                        <span className="absolute -left-5 top-2 h-3 w-5 rounded-xl rounded-r-none rounded-t-none border-b border-l bg-transparent hover:border-b-white hover:border-l-white" />
-                      </div>
-                      <div className="col-start-2 row-start-1 flex flex-col pl-4">
-                        <div className="flex gap-4">
-                          <div>Nickname</div>
-                          <div>Sent At</div>
-                        </div>
-                        <div>Role</div>
-                      </div>
-
-                      {childrenCommentHasChildren && (
-                        <Separator
-                          orientation="vertical"
-                          className="col-start-1 row-start-2 h-full justify-self-center"
-                        />
-                      )}
-
-                      <div className="col-start-2 row-start-2 flex flex-col gap-2 pl-4 pt-4">
-                        <div>Comment text</div>
-                        <div className="flex items-center gap-2">
-                          <PiThumbsUpBold /> <MessageCircleMore />
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                ) : null}
-              </>
-            );
-          })}
+          {renderRootComments(comments)}
           {/* <div className="col-start-1 col-end-3 row-start-1 grid grid-cols-[auto_1fr] items-center justify-self-start">
             <div className="col-start-1 row-start-1 h-10 w-10 rounded-full bg-foreground"></div>
             <div className="col-start-2 row-start-1 flex flex-col pl-4">
