bb0f4c982da34e16bea389b12256b1bd27f19dec
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-01-24 09:42:39 +0100

    Chat preview

--- apps/client/src/routes/_app/chats/index.tsx
+++ apps/client/src/routes/_app/chats/index.tsx
@@ -1,10 +1,53 @@
-import { createFileRoute } from "@tanstack/react-router";
-import Chat from "../../../pages/Chat";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import type { FC } from "react";
+import { InsetScrollArea } from "../../../components/custom/sidebar-wrapper";
+import { Avatar, AvatarImage } from "../../../components/ui/avatar";
 
 export const Route = createFileRoute("/_app/chats/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <Chat />;
+  return (
+    <InsetScrollArea>
+      <section className="grid min-h-[75vh] max-w-full flex-1 grid-cols-1 grid-rows-6 gap-4 rounded-xl bg-muted/50 p-4">
+        <ChatPreview
+          contactName="Nickname"
+          contactAvatar="https://github.com/shadcn.png"
+          lastMessage="Eu incididunt fugiat mollit nostrud aute cupidatat excepteur labore
+          duis laborum aliqua ullamco. Lorem dolore est qui mollit culpa. Sunt
+          eu mollit excepteur non veniam sint tempor irure dolore non tempor.
+          Sit laborum duis reprehenderit dolore officia elit aliqua. Anim eu
+          dolor labore officia do id veniam magna elit officia ex pariatur sunt."
+        />
+      </section>
+    </InsetScrollArea>
+  );
 }
+
+const ChatPreview: FC<{
+  contactName: string;
+  contactAvatar: string;
+  lastMessage: string;
+}> = ({ contactName, contactAvatar, lastMessage }) => {
+  return (
+    <Link
+      to={"/chats/$contact"}
+      params={{ contact: contactName }}
+      className="flex h-28 w-full items-center justify-between gap-8 rounded-xl bg-muted p-8 hover:bg-muted-foreground/30 hover:text-foreground"
+    >
+      <Avatar>
+        <AvatarImage
+          src={contactAvatar}
+          alt={`${contactName} profile picture`}
+        />
+      </Avatar>
+      <div className="flex max-w-[50%] flex-col items-end gap-3">
+        <div className="text-lg font-semibold">{contactName}</div>
+        <div className="line-clamp-1 font-semibold text-muted-foreground">
+          {lastMessage}
+        </div>
+      </div>
+    </Link>
+  );
+};
--- apps/client/src/routes/_app/marketplace/$category/index.tsx
+++ apps/client/src/routes/_app/marketplace/$category/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import type { FC } from "react";
 import { InsetScrollArea } from "../../../../components/custom/sidebar-wrapper";
 
 export const Route = createFileRoute("/_app/marketplace/$category/")({
@@ -10,27 +11,45 @@ function RouteComponent() {
   return (
     <InsetScrollArea>
       <section className="grid min-h-[75vh] max-w-full flex-1 grid-cols-1 grid-rows-6 rounded-xl bg-muted/50">
-        <Link
-          to="/marketplace/$category/$itemId"
-          params={{ category, itemId: "1" }}
-          className="group size-full p-4"
-        >
-          <div className="grid grid-cols-[auto_1fr] grid-rows-1 rounded-lg bg-muted">
-            <div className="justify-self-center p-4">
-              <div className="size-64 rounded-lg bg-white"></div>
-            </div>
-            <div className="grid h-full grid-cols-1 grid-rows-[auto_auto_1fr] items-start gap-3 p-8 pt-4">
-              <span className="text-4xl font-semibold group-hover:underline">
-                Camera
-              </span>
-              <span className="text-accent-foreground">
-                London - Today 10AM
-              </span>
-              <span className="pt-6 text-3xl font-semibold">$200</span>
-            </div>
-          </div>
-        </Link>
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
+        <ItemListing price={200} title="Camera" category={category} />
       </section>
     </InsetScrollArea>
   );
 }
+
+const ItemListing: FC<{ price: number; title: string; category: string }> = ({
+  price,
+  title,
+  category,
+}) => {
+  return (
+    <Link
+      to="/marketplace/$category/$itemId"
+      params={{ category, itemId: "1" }}
+      className="group size-full p-4"
+    >
+      <div className="grid grid-cols-[auto_1fr] grid-rows-1 rounded-lg bg-muted">
+        <div className="justify-self-center p-4">
+          <div className="size-64 rounded-lg bg-white"></div>
+        </div>
+        <div className="grid h-full grid-cols-1 grid-rows-[auto_auto_1fr] items-start gap-3 p-8 pt-4">
+          <span className="text-4xl font-semibold group-hover:underline">
+            {title}
+          </span>
+          <span className="text-accent-foreground">London - Today 10AM</span>
+          <span className="pt-6 text-3xl font-semibold">${price}</span>
+        </div>
+      </div>
+    </Link>
+  );
+};
