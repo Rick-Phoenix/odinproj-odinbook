458c229dd8a7b82b6cf405a046738082805b9104
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-01-24 08:58:32 +0100

    Category index page

--- apps/client/src/routes/_app/marketplace/$category/index.tsx
+++ apps/client/src/routes/_app/marketplace/$category/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { InsetScrollArea } from "../../../../components/custom/sidebar-wrapper";
 
 export const Route = createFileRoute("/_app/marketplace/$category/")({
@@ -6,9 +6,31 @@ export const Route = createFileRoute("/_app/marketplace/$category/")({
 });
 
 function RouteComponent() {
+  const { category } = Route.useParams();
   return (
     <InsetScrollArea>
-      <section className="grid min-h-[75vh] max-w-full grid-cols-1 grid-rows-[auto_1fr] rounded-xl bg-muted/50"></section>
+      <section className="grid min-h-[75vh] max-w-full flex-1 grid-cols-1 grid-rows-6 rounded-xl bg-muted/50">
+        <Link
+          to="/marketplace/$category/$itemId"
+          params={{ category, itemId: "1" }}
+          className="group size-full p-4"
+        >
+          <div className="grid grid-cols-[auto_1fr] grid-rows-1 rounded-lg bg-muted">
+            <div className="justify-self-center p-4">
+              <div className="size-64 rounded-lg bg-white"></div>
+            </div>
+            <div className="grid h-full grid-cols-1 grid-rows-[auto_auto_1fr] items-start gap-3 p-8 pt-4">
+              <span className="text-4xl font-semibold group-hover:underline">
+                Camera
+              </span>
+              <span className="text-accent-foreground">
+                London - Today 10AM
+              </span>
+              <span className="pt-6 text-3xl font-semibold">$200</span>
+            </div>
+          </div>
+        </Link>
+      </section>
     </InsetScrollArea>
   );
 }
