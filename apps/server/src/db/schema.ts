d65e404110c33ccafc15a0c4d1a387c36a038711
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-01-25 09:46:31 +0100

    More work on db

--- apps/server/src/db/schema.ts
+++ apps/server/src/db/schema.ts
@@ -11,6 +11,8 @@ import {
 } from "drizzle-orm/pg-core";
 import { lowercase, trim } from "../utils/db-methods";
 
+//
+
 export const usersTable = pgTable(
   "users",
   {
@@ -33,7 +35,13 @@ export const usersTable = pgTable(
 export const userRelations = relations(usersTable, ({ many }) => ({
   chats: many(chatInstancesTable),
   messages: many(messagesTable),
+  posts: many(postsTable),
+  comments: many(commentsTable),
+  roomsCreated: many(roomsTable, { relationName: "roomCreator" }),
+  roomsSubscribed: many(roomsTable, { relationName: "roomSubscribers" }),
 }));
+
+//
 
 export const chatsTable = pgTable("chats", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
@@ -44,6 +52,8 @@ export const chatsRelations = relations(chatsTable, ({ many }) => ({
   instances: many(chatInstancesTable),
   messages: many(messagesTable),
 }));
+
+//
 
 export const chatInstancesTable = pgTable(
   "chatInstances",
@@ -76,6 +86,8 @@ export const chatInstancesRelations = relations(
   })
 );
 
+//
+
 export const sessionsTable = pgTable("sessions", {
   id: text("id").primaryKey().notNull(),
   userId: text("userId")
@@ -86,6 +98,8 @@ export const sessionsTable = pgTable("sessions", {
     mode: "date",
   }).notNull(),
 });
+
+//
 
 export const messagesTable = pgTable("messages", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
@@ -110,6 +124,8 @@ export const messagesRelations = relations(messagesTable, ({ one }) => ({
   }),
 }));
 
+//
+
 export const listingsTable = pgTable("listings", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   userId: text("userId")
@@ -121,6 +137,8 @@ export const listingsTable = pgTable("listings", {
   location: text("location").notNull(),
 });
 
+//
+
 export const listingPicsTable = pgTable("listingPics", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   listingId: integer("listingId")
@@ -129,3 +147,72 @@ export const listingPicsTable = pgTable("listingPics", {
   url: text("url").notNull(),
   isThumbnail: boolean("isThumbnail"),
 });
+
+//
+
+export const roomsTable = pgTable("rooms", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: text("name").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  userId: text("userId").references(() => usersTable.id),
+});
+
+export const roomsRelations = relations(roomsTable, ({ many, one }) => ({
+  creator: one(usersTable, {
+    fields: [roomsTable.userId],
+    references: [usersTable.id],
+    relationName: "roomCreator",
+  }),
+  subscribers: many(usersTable, { relationName: "roomSubscribers" }),
+  posts: many(postsTable),
+}));
+
+//
+
+export const postsTable = pgTable("posts", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  roomId: integer("roomId")
+    .references(() => roomsTable.id, { onDelete: "cascade" })
+    .notNull(),
+  userId: text("userId").references(() => usersTable.id),
+  title: text("title").notNull(),
+  text: text("text").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
+
+export const postsRelations = relations(postsTable, ({ one, many }) => ({
+  author: one(usersTable, {
+    fields: [postsTable.userId],
+    references: [usersTable.id],
+  }),
+  comments: many(commentsTable),
+  room: one(roomsTable, {
+    fields: [postsTable.roomId],
+    references: [roomsTable.id],
+  }),
+}));
+
+//
+
+export const commentsTable = pgTable("comments", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  userId: text("userId").references(() => usersTable.id),
+  postId: integer("postId")
+    .references(() => postsTable.id, { onDelete: "cascade" })
+    .notNull(),
+  text: text("text").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
+
+export const commentsRelations = relations(commentsTable, ({ one, many }) => ({
+  author: one(usersTable, {
+    fields: [commentsTable.userId],
+    references: [usersTable.id],
+  }),
+  post: one(postsTable, {
+    fields: [commentsTable.postId],
+    references: [postsTable.id],
+  }),
+}));
+
+//
