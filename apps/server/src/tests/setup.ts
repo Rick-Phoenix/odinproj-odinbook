c1b585515a4fb80b61e64fb2cffcc485ed5020d5
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-03-07 15:56:09 +0100

    - Testing

--- apps/server/src/tests/setup.ts
+++ apps/server/src/tests/setup.ts
@@ -1,16 +1,31 @@
 import { afterAll, beforeAll, vi } from "vitest";
 
 beforeAll(() => {
-  vi.stubEnv("NODE_ENV", "test");
-  vi.stubEnv("GITHUB_CLIENT_ID", "");
-  vi.stubEnv("GITHUB_CLIENT_SECRET", "");
-  vi.stubEnv("GITHUB_CALLBACK_URI", "");
-  vi.stubEnv("SESSION_ENCRYPTION_KEY", "");
-  vi.stubEnv("CLOUDINARY_CLOUD_NAME", "");
-  vi.stubEnv("CLOUDINARY_API_KEY", "");
-  vi.stubEnv("CLOUDINARY_API_SECRET", "");
+  vi.mock("../types/env", () => {
+    return {
+      NODE_ENV: "test",
+      PORT: 3000,
+      DATABASE_URL: "",
+      LOG_LEVEL: "info",
+      GITHUB_CLIENT_ID: "",
+      GITHUB_CLIENT_SECRET: "",
+      GITHUB_CALLBACK_URI: "",
+      SESSION_ENCRYPTION_KEY: "",
+      CLOUDINARY_CLOUD_NAME: "",
+      CLOUDINARY_API_KEY: "",
+      CLOUDINARY_API_SECRET: "",
+    };
+  });
+  //vi.stubEnv("NODE_ENV", "test");
+  //vi.stubEnv("GITHUB_CLIENT_ID", "");
+  //vi.stubEnv("GITHUB_CLIENT_SECRET", "");
+  //vi.stubEnv("GITHUB_CALLBACK_URI", "");
+  //vi.stubEnv("SESSION_ENCRYPTION_KEY", "");
+  //vi.stubEnv("CLOUDINARY_CLOUD_NAME", "");
+  //vi.stubEnv("CLOUDINARY_API_KEY", "");
+  //vi.stubEnv("CLOUDINARY_API_SECRET", "");
 });
 
 afterAll(() => {
-  vi.unstubAllEnvs();
+  //vi.unstubAllEnvs();
 });
