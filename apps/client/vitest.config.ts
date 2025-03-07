686c3506dfe7b4aeb88d9d8850d562f82bc59cdb
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-03-07 16:01:26 +0100

    -

--- apps/client/vitest.config.ts
+++ apps/client/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    preserveSymlinks: true,
   },
   test: {
     globals: true,
--- apps/server/src/tests/setup.ts
+++ apps/server/src/tests/setup.ts
@@ -18,16 +18,6 @@ beforeAll(() => {
       },
     };
   });
-  //vi.stubEnv("NODE_ENV", "test");
-  //vi.stubEnv("GITHUB_CLIENT_ID", "");
-  //vi.stubEnv("GITHUB_CLIENT_SECRET", "");
-  //vi.stubEnv("GITHUB_CALLBACK_URI", "");
-  //vi.stubEnv("SESSION_ENCRYPTION_KEY", "");
-  //vi.stubEnv("CLOUDINARY_CLOUD_NAME", "");
-  //vi.stubEnv("CLOUDINARY_API_KEY", "");
-  //vi.stubEnv("CLOUDINARY_API_SECRET", "");
 });
 
-afterAll(() => {
-  //vi.unstubAllEnvs();
-});
+afterAll(() => {});
