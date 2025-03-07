c372856baf3ac690b9c289ebe88d4fc41fbfb32a
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-03-07 18:52:20 +0100

    - Fixed type errors on listings

--- packages/shared-schemas/dist/index.js
+++
@@ -1,5 +0,0 @@
-import { hc } from "hono/client";
-export { basicPostSchema, chatSchema, commentSchema, fullPostSchema, insertCommentSchema, insertListingSchema, insertMessageSchema, insertPostSchema, insertRoomSchema, insertUserSchema, itemConditions, listingSchema, loginValidationSchema, marketplaceCategories, messagesSchema, profileSchema, roomCategoriesArray, roomSchema, updatePasswordSchema, updateStatusSchema, userDataSchema, } from "@nexus/hono-api/schemas";
-// Api Client
-const client = hc("");
-export default (...args) => hc(...args);
